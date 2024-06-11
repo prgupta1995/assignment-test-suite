e57deee50aaaa7c39fb1ee95bed63140052fa575
Priyanka Gupta <priyanka.gupta881995@gmail.com>
2024-06-11 20:41:10 +0400

    initialize typescript and cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    specPattern: "**/*.feature",
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+      on("file:preprocessor", bundler);
+
+      return config;
     },
   },
 });
